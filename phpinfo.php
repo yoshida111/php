ef026b7f54755ef2cbaa0d8296f60fc0a8928cbe
ヨシダタクマ <63405574+yoshida111@users.noreply.github.com>
2020-08-18 18:17:45 +0900

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -1,3 +1,19 @@
 <?php
 phpinfo();
 ?>
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>お問い合わせフォーム</title>
+  </head>
+  <body>
+    <form action="./form.php" method="post">
+      名前<input type="text" name="fullname" value=""><br>
+      Eメール<input type="email" name="email" value=""><br>
+      お問い合わせ内容<br>
+      <textarea name="message" rows="8" cols="40"></textarea><br>
+      <input type="submit" name="confirm" value="確認">
+    </form>
+  </body>
+</html>
